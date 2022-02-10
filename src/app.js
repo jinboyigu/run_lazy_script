c40e3cfe53f1fb964e5700bfc5bcffd0231ca42b
jyhuang <jyhuang@coremail.cn>
2022-02-10 12:19:27 +0800

    perf: 增加私人仓库逻辑

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ const {execSync} = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-console.log('运行成功');
-
 async function main() {
+    const {token, ACTION_ENV: env} = process.env;
     const name = `lazy_script`;
     if (!fs.existsSync(name)) {
         console.time('git clone');
-        const output = execSync(`git clone https://github.com/jinboyigu/${name}.git`);
+        const url = token ? `https://jinboyigu:${token}@github.com/jinboyigu/${name}.git` : `https://github.com/jinboyigu/${name}.git`;
+        const output = execSync(`git clone ${url}`);
         console.log(output.toString());
         console.timeEnd('git clone');
     }
     console.time('run');
-    console.log(execSync(`cd ${name} && npm i && npm run dev`).toString());
+    console.log(execSync(`cd ${name} && npm i && npm run dev`, {env}).toString());
     console.timeEnd('run');
 }
 
+console.log('开始运行');
 main();
