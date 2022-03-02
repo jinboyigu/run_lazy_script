9b86931c537a532795702595f301a75cbefbd41d
jyhuang <jyhuang@coremail.cn>
2022-03-02 16:40:38 +0800

    test

--- src/app.js
+++ src/app.js
@@ -3,18 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
-    const {token, ACTION_ENV: env} = process.env;
-    const name = `lazy_script`;
-    if (!fs.existsSync(name)) {
-        console.time('git clone');
-        const url = token ? `https://jinboyigu:${token}@github.com/jinboyigu/${name}.git` : `https://github.com/jinboyigu/${name}.git`;
-        const output = execSync(`git clone ${url}`);
-        console.log(output.toString());
-        console.timeEnd('git clone');
-    }
-    console.time('run');
-    console.log(execSync(`cd ${name} && npm i && npm run dev`, {env}).toString());
-    console.timeEnd('run');
+    const {TEST} = process.env;
+    console.log('test', TEST === '123');
 }
 
 console.log('开始运行');
